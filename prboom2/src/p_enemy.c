a2ebbb2d8d4e6c7288ac8ad388e202ce34d8f1ec
JadingTsunami <JadingTsunami@users.noreply.github.com>
2026-05-03 18:32:58 -0700

    Implement MBF21 FLEEMELEE weapon flag.

--- prboom2/src/p_enemy.c
+++ prboom2/src/p_enemy.c
@@ -698,8 +698,7 @@ static void P_NewChaseDir(mobj_t *actor)
       actor->info->missilestate && actor->type != MT_SKULL &&
       ((!target->info->missilestate && dist < (actor->info->meleerange)*2) ||
        (target->player && dist < (actor->info->meleerange)*3 &&
-        (target->player->readyweapon == wp_fist ||
-         target->player->readyweapon == wp_chainsaw))))
+        (weaponinfo[target->player->readyweapon].mbf21weaponflags & WF_FLEEMELEE))))
     {       // Back away from melee attacker
       actor->strafecount = P_Random(pr_enemystrafe) & 15;
       deltax = -deltax, deltay = -deltay;
