05a8d371c84688db1caa78527b7775d26e502cd2
JadingTsunami <JadingTsunami@users.noreply.github.com>
2026-08-10 10:13:29 -0700

    Fix npo2 textures.

--- prboom2/src/r_data.c
+++ prboom2/src/r_data.c
@@ -103,11 +103,16 @@ int       *texturetranslation;
 //
 
 const byte *R_GetTextureColumn(const rpatch_t *texpatch, int col) {
-  while (col < 0)
-    col += texpatch->width;
-  col &= texpatch->widthmask;
-  
-  return texpatch->columns[col].pixels;
+    while (col < 0)
+        col += texpatch->width;
+
+    if (texpatch->widthmask == texpatch->width-1) {
+        col &= texpatch->widthmask;
+    } else {
+        col %= texpatch->width;
+    }
+
+    return texpatch->columns[col].pixels;
 }
 
 //
